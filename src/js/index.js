b13ccda915ae75d0e729be20f4985d9d382141a3
Steven Chang <1632460272@qq.com>
2019-12-31 16:03:10 +0000

    Configuration

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,14 @@
 // Global app controller
-import x from "./test"
-console.log(`Guanpi ${x}`)
+import axios from "axios"
+
+async function getResult(query){
+    try{
+        const res = await axios(`https://forkify-api.herokuapp.com/api/search?&q=${query}`);
+        console.log(res)
+        const recipe = res.data.recipes;
+        console.log(recipe);
+    } catch(error){
+        console.log(error)
+    }  
+} 
+getResult('pizza');  
--- src/js/test.js
+++
@@ -1,2 +0,0 @@
-console.log("test");
-export default 1193;
